ba75015fc6f2f0237a578b9f6b28b2f31154538e
Nikita Klimcuks <nikita.klimchuck@gmail.com>
2024-01-26 22:25:36 +0000

    create List component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import NavBar from "./components/NavBar";
 import Box from "./components/Box";
 import Summary from "./components/Summary";
+import List from "./components/List";
 
 const tempMovieData = [
   {
@@ -82,28 +83,7 @@ export default function App() {
             <>
               <Summary watched={watched} avgImdbRating={avgImdbRating} avgUserRating={avgUserRating} avgRuntime={avgRuntime}/>
               
-              <ul className="list">
-                {watched.map((movie) => (
-                  <li key={movie.imdbID}>
-                    <img src={movie.Poster} alt={`${movie.Title} poster`} />
-                    <h3>{movie.Title}</h3>
-                    <div>
-                      <p>
-                        <span>⭐️</span>
-                        <span>{movie.imdbRating}</span>
-                      </p>
-                      <p>
-                        <span>🌟</span>
-                        <span>{movie.userRating}</span>
-                      </p>
-                      <p>
-                        <span>⏳</span>
-                        <span>{movie.runtime} min</span>
-                      </p>
-                    </div>
-                  </li>
-                ))}
-              </ul>
+              <List watched={watched}/>
             </>
           )}
         </div>
